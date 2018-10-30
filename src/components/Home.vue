0044f867896612248017613a94447ec731f4df73
jolaolu <adebayoanjola@gmail.com>
2018-10-30 17:18:31 +0100

    legbe legbe💪💪💪   💪

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,46 +31,47 @@
         </div>
       </div>
       <hr>
-      <div class="columns pd">
-        <div class="column">
-          <div class="card">
-            <div class="card-contents">
-              <p class="title">
-                “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-              </p>
-              <p class="subtitle">
-                Jeff Atwood
-              </p>
+      <div class="container">
+        <div class="columns pd">
+          <div class="column">
+            <div class="card">
+              <div class="card-contents">
+                <p class="title">
+                  “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+                </p>
+                <p class="subtitle">
+                  Jeff Atwood
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-contents">
+                <p class="title">
+                  “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+                </p>
+                <p class="subtitle">
+                  Jeff Atwood
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">
+                  “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+                </p>
+                <p class="subtitle">
+                  --
+                  Jeff Atwood
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div class="column">
-          <div class="card">
-            <div class="card-contents">
-              <p class="title">
-                “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-              </p>
-              <p class="subtitle">
-                Jeff Atwood
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="card">
-            <div class="card-contents">
-              <p class="subtitle">
-                “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-              </p>
-              <p class="subtitle">
-                --
-                Jeff Atwood
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
+    </div>
     </section>
   </div>
 </template>
@@ -105,14 +106,22 @@ export default {
       margin-top: 2rem
 
 .pd
-  padding: 2rem
+  +tablet
+  padding: 2rem 0
   +desktop
     font-weight: normal
 
 h2
   margin: 2rem 2rem 0 !important
+  padding-bottom: 2rem
 
 .fa-cog
   font-size: 40px
-  
+
+#learn
+  +desktop
+    margin-bottom: 2rem
+
+
+
 </style>
